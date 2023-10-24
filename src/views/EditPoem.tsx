934c55c5abd38dbce1b1e8a0153b71af79c32e26
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-10-23 23:56:37 -0500

    [add][fix] implemented new user card as well as bug fixes

--- src/views/EditPoem.tsx
+++ src/views/EditPoem.tsx
@@ -2,35 +2,40 @@ import { useParams, useNavigate } from 'react-router-dom';
 import { getPoems, editPoem } from '../utils/poemManager';
 import RichTextEditor from '../components/RichTextEditor/RichTextEditor';
 import { useState } from 'react';
-import '../styles/EditPoem.scss'
+import '../styles/EditPoem.scss';
 
 function EditPoem() {
-  const { id } = useParams();
-  const poemToEdit = getPoems().find(poem => poem.id === id);
-  
-  const [title, setTitle] = useState(poemToEdit?.title || '');
-  const [content, setContent] = useState(poemToEdit?.content || '');
+    const { id } = useParams();
+    const navigate = useNavigate();
 
-  const navigate = useNavigate();
+    const poemToEdit = getPoems().find(poem => poem.id === id);
 
-  const handleUpdate = () => {
-    const updatedPoem = {
-      id,
-      title,
-      content
+    const [title, setTitle] = useState(poemToEdit?.title || '');
+    const [content, setContent] = useState(poemToEdit?.content || '');
+
+    if (!id || !poemToEdit) {
+        console.error("No se proporcionó un ID válido o el poema no existe.");
+        return null;
+    }
+
+    const handleUpdate = () => {
+        const updatedPoem = {
+            id,
+            title,
+            content
+        };
+        editPoem(updatedPoem);
+        navigate('/');
     };
-    editPoem(updatedPoem);
-    navigate('/');
-  };
 
-  return (
-    <div className="edit-container">
-      <h1>Editar Poema</h1>
-      <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Título" />
-      <RichTextEditor content={content} setContent={setContent} />
-      <button onClick={handleUpdate}>Actualizar</button>
-    </div>
-  );
+    return (
+        <div className="edit-container">
+            <h1>Editar Poema</h1>
+            <input value={title} onChange={e => setTitle(e.target.value)} placeholder="Título" />
+            <RichTextEditor content={content} setContent={setContent} />
+            <button onClick={handleUpdate}>Actualizar</button>
+        </div>
+    );
 }
 
-export default EditPoem
+export default EditPoem;
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PoemList from '../components/PoemList/PoemList';
-import '../styles/Home.scss'
+import { getPoems } from '../utils/poemManager'
+import '../styles/Home.scss';
 
 function Home() {
-  return (
-    <div className="home-container">
-      <h1>Listado de Poemas</h1>
-      <PoemList />
-    </div>
-  );
+    const [hasPoems, setHasPoems] = useState(true);
+
+    useEffect(() => {
+        const poems = getPoems();
+        if(poems.length === 0) {
+            setHasPoems(false);
+        }
+    }, []);
+
+    return (
+        <div className="home-container">
+            <h1>Listado de Poemas</h1>
+            { hasPoems ? (
+                <PoemList />
+            ) : (
+                <Link to="/create" className="create-poem-card">
+                    <h2>Crear Nuevo Poema</h2>
+                    <p>Haz click aquí para empezar</p>
+                </Link>
+            )}
+        </div>
+    );
 }
 
 export default Home;
