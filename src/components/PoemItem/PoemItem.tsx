c6fe8d22bafd7e4094874a7e3827735753a66ec6
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-10-23 19:51:30 -0500

    [add][fix] implemented display mode for the cards

--- src/components/PoemItem/PoemItem.tsx
+++ src/components/PoemItem/PoemItem.tsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom';
 import { deletePoem } from '../../utils/poemManager';
 import { PoemItemProps } from '../../models/Poem';
 import './PoemItem.scss';
+import { useState } from 'react';
 
 function PoemItem({ poem }: PoemItemProps) {
+  const [showModal, setShowModal] = useState(false);
 
   const contentPreview = poem.content.slice(0, 100);
 
@@ -12,8 +14,16 @@ function PoemItem({ poem }: PoemItemProps) {
     window.location.reload();
   };
 
+  const handleOpenModal = () => {
+    setShowModal(true);
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
+
   return (
-    <div className='poemItem-container'>
+    <div className='poemItem-container' onClick={handleOpenModal}>
       <h2>{poem.title}</h2>
       <div
         className="poem-content-preview"
@@ -21,6 +31,19 @@ function PoemItem({ poem }: PoemItemProps) {
       ></div>
       <Link to={`/edit/${poem.id}`}>Editar</Link>
       <button onClick={handleDelete}>Eliminar</button>
+
+      {showModal && (
+        <div className="modal visible" onClick={handleCloseModal}>
+          <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+            <span className="close-btn" onClick={handleCloseModal}>&times;</span>
+            <h2>{poem.title}</h2>
+            <div
+              dangerouslySetInnerHTML={{ __html: poem.content }}
+            ></div>
+          </div>
+        </div>
+      )}
+
     </div>
   );
 }
