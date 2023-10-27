f2c6236ebf8d57ab08ec58b48800846a8dc5445e
Daniel-Santiago-Acosta-1013 <santiagoacosta1013@gmail.com>
2023-10-26 19:13:28 -0500

    [add][fix] improving design with small screens

--- src/components/RichTextEditor/RichTextEditor.tsx
+++ src/components/RichTextEditor/RichTextEditor.tsx
@@ -1,12 +1,24 @@
 import ReactQuill from 'react-quill';
 import { RichTextEditorProps } from '../../models/Poem'
-import 'react-quill/dist/quill.snow.css'; // import styles
+import 'react-quill/dist/quill.snow.css'; 
 import './RichTextEditor.scss';
 
 function RichTextEditor({ content, setContent }: RichTextEditorProps) {
+  const modules = {
+    toolbar: [
+      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+      [{size: []}],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{'list': 'ordered'}, {'list': 'bullet'}, 
+       {'indent': '-1'}, {'indent': '+1'}],
+      ['link', 'image', 'video'],
+      ['clean']                                            
+    ],
+  };
+
   return (
     <div className='richTextEditor-container'>
-      <ReactQuill value={content} onChange={setContent} />
+      <ReactQuill value={content} onChange={setContent} modules={modules} />
     </div>
   );
 }
